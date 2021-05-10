269c24eca471c573df919686fadbd32633953a85
conradtrost <ct.conradtrost@gmail.com>
2021-05-10 15:34:56 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteMetadata: {
+    title: "ottinsurance.agency",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-0BT1GNGD1R",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
